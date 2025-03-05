e5dcd8f417d6f2c7f1f029e685fc4606e7004716
de4nde <131461297+de4nde@users.noreply.github.com>
2025-03-05 13:28:56 +0100

    Improve HTTP connections with persistent connections and rate limiting

--- lib/wayback_machine_downloader/archive_api.rb
+++ lib/wayback_machine_downloader/archive_api.rb
@@ -1,40 +1,36 @@
 require 'json'
-require 'uri'
+require 'open-uri'
 
 module ArchiveAPI
-
-  def get_raw_list_from_api url, page_index
-    request_url = URI("https://web.archive.org/cdx/search/xd")
+  def get_raw_list_from_api(url, page_index)
+    request_url = URI("https://web.archive.org/cdx/search/cdx")
     params = [["output", "json"], ["url", url]]
-    params += parameters_for_api page_index
+    params += parameters_for_api(page_index)
     request_url.query = URI.encode_www_form(params)
-
+    
     begin
-      json = JSON.parse(URI(request_url).open.read)
-      if (json[0] <=> ["timestamp","original"]) == 0
-        json.shift
+      open(request_url) do |response|
+        json = JSON.parse(response.read)
+        if (json[0] <=> ["timestamp","original"]) == 0
+          json.shift
+        end
+        json
       end
-      json
-    rescue JSON::ParserError
+    rescue OpenURI::HTTPError, JSON::ParserError => e
+      puts "Error fetching data: #{e.message}"
+      []
+    rescue SocketError => e
+      puts "Network error: #{e.message}"
       []
     end
   end
-
-  def parameters_for_api page_index
+  
+  def parameters_for_api(page_index)
     parameters = [["fl", "timestamp,original"], ["collapse", "digest"], ["gzip", "false"]]
-    if !@all
-      parameters.push(["filter", "statuscode:200"])
-    end
-    if @from_timestamp and @from_timestamp != 0
-      parameters.push(["from", @from_timestamp.to_s])
-    end
-    if @to_timestamp and @to_timestamp != 0
-      parameters.push(["to", @to_timestamp.to_s])
-    end
-    if page_index
-      parameters.push(["page", page_index])
-    end
+    parameters.push(["filter", "statuscode:200"]) unless @all
+    parameters.push(["from", @from_timestamp.to_s]) if @from_timestamp && @from_timestamp != 0
+    parameters.push(["to", @to_timestamp.to_s]) if @to_timestamp && @to_timestamp != 0
+    parameters.push(["page", page_index]) if page_index
     parameters
   end
-
 end
